955a70a6b1c9f8afa5827e5f56705ac4b006f275
mrnestor123 <barrachinasanernest@gmail.com>
2023-06-12 13:09:05 +0200

    añadido

--- GeneradorNumero/generador.js
+++ GeneradorNumero/generador.js
@@ -1,8 +1,12 @@
 function generateRandomNumber() {
-    var minValue = parseInt(document.getElementById("minValue").value);
-    var maxValue = parseInt(document.getElementById("maxValue").value);
+    const minValue = parseInt(document.getElementById("minValue").value);
+    const maxValue = parseInt(document.getElementById("maxValue").value);
 
-    var randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+    if (isNaN(minValue) || isNaN(maxValue) || minValue >= maxValue) {
+        document.getElementById("result").innerHTML = "Error: Invalid input";
+        return;
+    }
 
+    const randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
     document.getElementById("result").innerHTML = "Número aleatorio: " + randomNumber;
 }

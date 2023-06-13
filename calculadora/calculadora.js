8798185ab9c6017fc9930a08887494be9db47c4c
mrnestor123 <barrachinasanernest@gmail.com>
2023-06-13 14:04:58 +0200

    modificar funciones

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,30 +1,67 @@
 var userInput = "";
+var cursorPosition = 0;
 
 function calculate() {
   var expression = document.getElementById("userInput").textContent;
-  var result = eval(expression);
-  document.getElementById("result").value = result;
+  var result;
+
+  try {
+    expression = expression.replace(/[^-()\d/*+.]/g, '');
+    result = eval(expression);
+    document.getElementById("result").value = result;
+  } catch (error) {
+    document.getElementById("result").value = "ಠ_ಠ";
+    console.error(error);
+  }
 }
 
 function appendInput(value) {
-  userInput += value;
-  document.getElementById("userInput").textContent = userInput;
+  userInput = userInput.slice(0, cursorPosition) + value + userInput.slice(cursorPosition);
+  cursorPosition++;
+  updateDisplay();
 }
 
 function cleanInputs() {
   userInput = "";
-  document.getElementById("userInput").textContent = userInput;
+  cursorPosition = 0;
+  updateDisplay();
   document.getElementById("result").value = "";
 }
 
 function deleteLastInput() {
-  userInput = userInput.slice(0, -1);
-  document.getElementById("userInput").textContent = userInput;
+  if (cursorPosition > 0) {
+    userInput = userInput.slice(0, cursorPosition - 1) + userInput.slice(cursorPosition);
+    cursorPosition--;
+    updateDisplay();
+  }
+}
+
+function moveCursorLeft() {
+  if (cursorPosition > 0) {
+    cursorPosition--;
+    updateDisplay();
+  }
+}
+
+function moveCursorRight() {
+  if (cursorPosition < userInput.length) {
+    cursorPosition++;
+    updateDisplay();
+  }
+}
+
+function updateDisplay() {
+  var userInputDisplay = userInput.slice(0, cursorPosition) + "|" + userInput.slice(cursorPosition);
+  document.getElementById("userInput").textContent = userInputDisplay;
+
+  var userInputContainer = document.getElementById("userInput");
+  userInputContainer.scrollLeft = userInputContainer.scrollWidth;
 }
 
 document.addEventListener("keydown", function(event) {
   var key = event.key;
-
+  event.preventDefault();
+  
   if (!isNaN(key) || key === "+" || key === "-" || key === "*" || key === "/") {
     appendInput(key);
   }
@@ -55,5 +92,13 @@ document.addEventListener("keydown", function(event) {
 
   if (key === ",") {
     appendInput(",");
+  }
+
+  if (key === "ArrowLeft") {
+    moveCursorLeft();
+  }
+
+  if (key === "ArrowRight") {
+    moveCursorRight();
   }
 });
